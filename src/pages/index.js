8afff8fe7743603f008370ff5a34f4c81ab93385
jordan-s-ramirez <jordan.s.ramirez.com>
2023-03-03 20:02:24 -0500

    timer tweak

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import React,{useState} from "react";
 import Controls from "./components/controls";
 import Display from "./components/display";
 import Results from "./components/results";
+import { motion } from "framer-motion";
 import './global.css'
 
 export default function IndexPage() {
-  const [mainString, setMainString] = ("This is the test string")
+  const [mainString, setMainString] = useState("This is the test string")
   const [start, setStart] = useState(false)
   const [finish, setFinish] = useState(false)
   const [data, setData] = useState([])
@@ -14,31 +15,37 @@ export default function IndexPage() {
   const [accuracy, setAccuracy] = useState(0)
 
   function handleFinish(acc) {
-    setFinish(true)
-    setAccuracy(acc)
+    // End Time
+    var endTime = new Date()
 
     // Calculate words per min
-    var typeTime = new Date() - startTime
-    var mins = (typeTime / 1000) / 60
+    var typeTime = endTime.getTime()- startTime.getTime()
+    var mins = (typeTime / 50) / 60
     var wordsPerMin = mainString.split(" ").length / mins
+    console.log(typeTime / 50)
+
+    // Setting varibles
     setWpm(Math.floor(wordsPerMin))
+    setFinish(true)
+    setAccuracy(acc)
   }
 
   function handleClick() {
     // Pick an random string
-    var str = "This is the test string"
-    var strArray = str.split("")
-
+    var strTest = "This is the test string, I hope this works"
+    var strArray = strTest.split("")
+    
     // Configure string data
     var strData = []
     strArray.forEach(e =>{
       strData.push(e)
     })
-
+    
     // Initalize Start
     setData(strData)
+    setMainString(strTest)
     setStart(true)
-    
+
     // Start Timer
     setStartTime(new Date())
   }
@@ -53,7 +60,24 @@ export default function IndexPage() {
         <></>
       )}
       {finish?(
-        <Results wpm={wpm} accuracy={accuracy}/>
+        <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          default: {
+            duration: 0.1,
+            ease: [0, 0.71, 0.2, 1.01]
+          },
+          scale: {
+            type: "linear",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001
+          }
+        }}
+        >
+          <Results wpm={wpm} accuracy={accuracy}/>
+        </motion.div>
       ):(
         <></>
       )}
